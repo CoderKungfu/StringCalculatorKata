5de73ac56f4ec4d26854fc214d490f0e70647cfa
Michael Cheng <michael_cheng@tech.gov.sg>
2020-04-12 16:52:49 +0800

    Implement Step 1

--- add.js
+++ add.js
@@ -1,2 +1,20 @@
-const add = (a, b) => a + b
+const EXPECTED_NUMS = 2
+const DELIMITER = ','
+
+const isValidInput = (input) => {
+  if(typeof input !== 'string') return false
+  if(input.length < 1) return false
+
+  return true
+}
+
+const add = (numbers) => {
+  if (!isValidInput(numbers)) return 0
+
+  const intArray = numbers.split(DELIMITER)
+  if (intArray.length == EXPECTED_NUMS)
+    return Number(intArray[0]) + Number(intArray[1])
+  else
+    return Number(intArray[0])
+}
 export default add
--- add.test.js
+++ add.test.js
@@ -1,5 +1,21 @@
 import add from './add'
 
-test('adds 1 + 2 to equal 3', () => {
-  expect(add(1, 2)).toBe(3);
-});
+test('empty string should return 0', () => {
+  expect(add('')).toBe(0)
+})
+
+test('empty null should return 0', () => {
+  expect(add(null)).toBe(0)
+})
+
+test('empty string with spaces should return 0', () => {
+  expect(add('    ')).toBe(0)
+})
+
+test("string of '1' should return 1", () => {
+  expect(add('1')).toBe(1)
+})
+
+test("string of '1,2' should return 3", () => {
+  expect(add('1,2')).toBe(3)
+})
